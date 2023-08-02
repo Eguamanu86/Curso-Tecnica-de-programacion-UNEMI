3b198f44175bef271021a93c9ad1960c2a912ff6
Ernesto Guaman <ernesto.guaman@enviame.io>
2023-08-01 20:39:56 -0500

    casos de uso gestion bancario

--- 2doParcial/caso-estudio-gestion-bancaria/src/repository/cliente-repository.js
+++ 2doParcial/caso-estudio-gestion-bancaria/src/repository/cliente-repository.js
@@ -14,7 +14,7 @@ export class ClienteRespository {
         c.telefono
       )
     })
-    // this.clientes = [Cliente(),Cliente()]
+    // this.clientes = [Cliente(),Cliente().... n]
   }
 
   //get: retornar valores
@@ -25,6 +25,7 @@ export class ClienteRespository {
   // set: asignar valores o crear
   setCreate(cliente) {             // 0 ,     1  ,     2
     this.clientes.push(cliente) // [Cliente1,Cliente2,Cliente(end)... n]
+    //                     JSON  [{nombre: "ernesto"}]
     //                     key    ,  JSON: [{"nombre":"ernesto"},{}]
     localStorage.setItem('clientes', JSON.stringify(this.clientes))
   }
